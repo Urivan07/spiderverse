09f6a6d52b08ed12088745b5ec31d67db4e0596f
Urivan <urivan07@gmail.com>
2022-04-30 13:58:30 -0500

    Add - index de proyecto

---
+++ index.js
@@ -0,0 +1,7 @@
+const Spiderman = require("./app/spiderman")
+
+const spiderman = new Spiderman('Spider Man', '46', 'Tobey Maguire', '3', 'Columbia Pictures​')
+
+console.log("Spiderman")
+console.log("Edad", spiderman.actor)
+console.log("Edad", spiderman.edad)
